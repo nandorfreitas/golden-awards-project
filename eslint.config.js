e00f5019703b9481786755ca9b14f8166a4af077
nandorfreitas <fernandofreitas.ufrrj@gmail.com>
2025-01-28 19:22:56 -0300

    chore: add new configs

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,16 @@ import pluginJs from "@eslint/js";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser, parserOptions: { sourceType: "module" }}},
+  {
+		languageOptions: {
+			globals: globals.browser,
+			parserOptions: { sourceType: "module" },
+			overrides: [
+				{
+					"env": {
+						"jest": true
+					}
+				}
+  		]}},
   pluginJs.configs.recommended,
 ];
